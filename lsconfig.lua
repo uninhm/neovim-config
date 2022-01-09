01de9a03c0eaf3eca6d4d92ffafc4c94134ee720
Unai Hernández Minaberry <unihernandez22@gmail.com>
2022-01-09 15:35:10 -0300

    add hls

--- lsconfig.lua
+++ lsconfig.lua
@@ -33,7 +33,7 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
-local servers = { 'pyright', 'clangd', 'nimls' }
+local servers = { 'pyright', 'clangd', 'nimls', 'hls' }
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
