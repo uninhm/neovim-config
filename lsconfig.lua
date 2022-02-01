2724e151fdb9ba2c5c81d71adcfb47d7371669f2
Unai Hernández Minaberry <unihernandez22@gmail.com>
2022-01-31 21:23:54 -0300

    add rust-analyzer LS

--- lsconfig.lua
+++ lsconfig.lua
@@ -33,7 +33,7 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
-local servers = { 'pyright', 'clangd', 'nimls', 'hls' }
+local servers = { 'pyright', 'clangd', 'nimls', 'hls', 'rust_analyzer' }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
